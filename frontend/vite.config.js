7e892f0f30449adda3478b35b0b7ed6ab2584693
Mohit Gahlyan <dev@starkedge.com>
2025-10-24 16:46:22 +0530

    update vite.config for deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_BACKEND_URL,
-        changeOrigin: true,
+export default ({ mode }) => {
+  // load env file based on `mode` (development, production, etc.)
+  // the third parameter '' ensures we get raw keys (not only VITE_ prefix)
+  const env = loadEnv(mode, process.cwd(), '')
+  const backend = env.VITE_BACKEND_URL || 'http://localhost:5000'
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: backend,
+          changeOrigin: true,
+        },
       },
     },
-  },
-})
+  })
+}
